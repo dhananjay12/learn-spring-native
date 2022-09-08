c4b68b1e62fdd93cb0be1c3f6a9ccaf6e7794daa
Dhananjay Singh <dhananjay@backbase.com>
2022-09-08 21:26:21 +0200

    example

--- database-app/src/main/java/com/example/demo/DatabaseAppApplication.java
+++ database-app/src/main/java/com/example/demo/DatabaseAppApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@TypeHints(
+//        @TypeHint(typeNames = "org.hibernate.dialect.MySQL8Dialect", types = MySQL8Dialect.class) // Need to specify if using MySQL5InnoDB
+//)
 public class DatabaseAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatabaseAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseAppApplication.class, args);
+    }
 
 }
